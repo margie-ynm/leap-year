49cd9961dc56c85fc70b9d545e48495af66114f6
Jim Padilla and Margie Nevarez <margie@>
2016-10-26 09:10:07 -0700

    Add spec where year % 100 should be 0

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+//business logic
+var calculator = function(squirrel) {
+  if ((squirrel % 4 === 0) && (squirrel % 100 !== 0))  {
+    return true;
+  } else {
+    return false;
+  }
+
+};
+
+
+
+//user-interface
+$(document).ready(function() {
+  $("form#leap-form").submit(function(event) {
+    event.preventDefault();
+    var yearInput = parseInt($("#leap-year").val());
+    var result = calculator(yearInput);
+    $("#result p").text(result);
+  });
+});
---
+++ readme.md
@@ -0,0 +1,8 @@
+
+Specs
+
+Behavior                       | Input  | Output
+Year that is not a leap year      1993    false
+Year that is divisible by 4       2004    true
+Year that is divisible by 100     1900    false
+Year that is divisible by 400     2000    true
